d386aba688fa8aee3315e89f462ba2448cb1372e
JAL SINGH YADAV <yadavjalsingh192@gmail.com>
2026-09-21 22:15:48 +0530

    feat(document): export ACL subject matcher

--- src/core/index.ts
+++ src/core/index.ts
@@ -46,3 +46,4 @@ export * from "./api/core-operation-contracts.js";
 export * from "./integration/event-envelope.js";
 export * from "./document/access-candidate.js";
 export * from "./document/acl.js";
+export * from "./document/acl-subject-match.js";
