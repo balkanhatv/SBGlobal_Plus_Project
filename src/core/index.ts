0a7c6165dabd4224d80cc21dc631dd9a2c469458
JAL SINGH YADAV <yadavjalsingh192@gmail.com>
2026-09-22 08:10:50 +0530

    feat(integration): export ProviderAdapter contract

--- src/core/index.ts
+++ src/core/index.ts
@@ -54,3 +54,4 @@ export * from "./integration/outbox-event.js";
 export * from "./integration/event-catalog.js";
 export * from "./integration/integration-definition.js";
 export * from "./integration/integration-capability.js";
+export * from "./integration/provider-adapter.js";
