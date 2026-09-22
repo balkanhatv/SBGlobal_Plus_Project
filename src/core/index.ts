25c716ed7313d20f7f38482ed4dc1012522b578e
JAL SINGH YADAV <yadavjalsingh192@gmail.com>
2026-09-22 08:52:00 +0530

    feat(integration): export SyncCursor contract

--- src/core/index.ts
+++ src/core/index.ts
@@ -57,3 +57,4 @@ export * from "./integration/integration-capability.js";
 export * from "./integration/provider-adapter.js";
 export * from "./integration/tenant-integration.js";
 export * from "./integration/credential-reference-metadata.js";
+export * from "./integration/sync-cursor.js";
